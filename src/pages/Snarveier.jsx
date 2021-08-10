3e7446f5127d38f7c143e0c42d5cb67e6b70cc5c
vegardinho <landsverk.vegard@gmail.com>
2021-08-10 23:48:08 +0200

    Scroll without shadow

--- src/pages/Snarveier.jsx
+++ src/pages/Snarveier.jsx
@@ -11,72 +11,23 @@ const useStyles = makeStyles({
     maxHeight: '30vh',
     overflow: 'auto',
     placeItems: 'flex-start',
-    pointerEvents: 'all',
-    // width: '60vw'
+    // background: 'linear-gradient(rgba(255, 200, 176, 1) 30%, rgba(255, 200, 176, 0)), linear-gradient(rgba(255, 200, 176, 0), rgba(255, 200, 176, 1) 70%) 0 100%, radial-gradient(farthest-side at 50% 0, rgba(0, 0, 0, .2), rgba(0, 0, 0, 0)), radial-gradient(farthest-side at 50% 100%, rgba(0, 0, 0, .2), rgba(0, 0, 0, 0)) 0 100%',
+    // backgroundColor: 'transparent',
+    // backgroundRepeat: 'no-repeat',
+    // backgroundSize: '100% 40px, 100% 40px, 100% 14px, 100% 14px',
+    // backgroundAttachment: 'local, local, scroll, scroll',
+
   },
-  overlay: {
-    position: 'absolute',
-    maxHeight: '30vh',
-    width: '90%',
-    height: '30vh',
-    pointerEvents: 'none',
-    transition: 'background 1s',
-    // background: ({ topReached, bottomReached }) => {
-    //   if (topReached) {
-    //     return 'linear-gradient(to bottom, rgba(255, 200, 176,0) 70%, rgba(255, 200, 176,1) 100%)';
-    //   } if (bottomReached) {
-    //     return 'linear-gradient(to top, rgba(255, 200, 176,0) 70%, rgba(255, 200, 176,1) 100%)';
-    //   }
-    //   return 'linear-gradient(to bottom, rgba(255, 200, 176,0) 70%, rgba(255, 200, 176,1) 100%),linear-gradient(to top, rgba(255, 200, 176,0) 70%, rgba(255, 200, 176,1) 100%)';
-    // },
-  },
+
 });
 
 export default function Snarveier() {
-  const [topReached, setTopReached] = useState(true);
-  const [bottomReached, setBottomReached] = useState(false);
-  const classes = useStyles({ topReached, bottomReached });
-
-  const handleScroll = (e) => {
-    const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
-    const top = e.target.scrollTop === 0;
-
-    if (top) {
-      setTopReached(true);
-      setBottomReached(false);
-    } else if (bottom) {
-      setBottomReached(true);
-      setTopReached(false);
-    } else {
-      setTopReached(false);
-      setBottomReached(false);
-    }
-  };
-
-  const overlay = (direction) => `linear-gradient(${direction}, rgba(255, 200, 176,0) 70%, rgba(255, 200, 176,1) 100%)`;
-
-  const returnOverlay = (top, bottom) => {
-    if (top) {
-      // return overlay('to bottom');
-      return 'linear-gradient(to bottom, rgba(255,200,176,0) 0%, rgba(255,200,176,0) 30%, rgba(255,200,176,0) 70%, rgba(255,200,176,1) 100%)';
-    } if (bottom) {
-      return 'linear-gradient(to bottom, rgba(255,200,176,1) 0%, rgba(255,200,176,0) 30%, rgba(255,200,176,0) 70%, rgba(255,200,176,0) 100%)';
-    }
-    // return `${overlay('to bottom')}, ${overlay('to top')}`;
-    return 'linear-gradient(to bottom, rgba(255, 200, 176,1) 0%, rgba(255, 200, 176,0) 30%, rgba(255, 200, 176,0) 70%, rgba(255, 200, 176,1) 100%)';
-  };
+  const classes = useStyles();
 
   return (
     <PageTemplate image={cow} imageCaption="schmelk-kua">
-      <div onScroll={(e) => handleScroll(e)} className={classes.buttonList}>
-        <ButtonList items={snarveier}>
-          <motion.div
-            initial={{ background: returnOverlay(topReached, bottomReached) }}
-            animate={{ background: returnOverlay(topReached, bottomReached) }}
-            transition={{ background: { delay: 0 } }}
-            className={classes.overlay}
-          />
-        </ButtonList>
+      <div className={classes.buttonList}>
+        <ButtonList items={snarveier} />
       </div>
     </PageTemplate>
   );
